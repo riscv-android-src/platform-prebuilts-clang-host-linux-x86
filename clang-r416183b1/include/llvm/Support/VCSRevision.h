45108a988e6e3b65cad65e449669568f94efb84a
Mao Han <han_mao@linux.alibaba.com>
2022-06-22 16:21:14 +0800

    Update clang to fix lto and smart-limit

--- clang-r416183b1/include/llvm/Support/VCSRevision.h
+++ clang-r416183b1/include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "b2ea719685eeb6410a4a0d7ccdec1808e637fadc"
+#define LLVM_REVISION "ad485e8fa484708b198a89fb57e055158b3b4128"
 #define LLVM_REPOSITORY "/home/maoh/android_clang/out/llvm-project/llvm"
