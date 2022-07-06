c71b5fcfb9b5c76475d445d2fedef994bc1881b0
Mao Han <han_mao@linux.alibaba.com>
2022-07-06 17:04:37 +0800

    Update clang to fix bionic test

--- clang-r416183b1/include/llvm/Support/VCSRevision.h
+++ clang-r416183b1/include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "ad485e8fa484708b198a89fb57e055158b3b4128"
-#define LLVM_REPOSITORY "/home/maoh/android_clang/out/llvm-project/llvm"
+#define LLVM_REVISION "f1f566c3d0ff34cd8f841859372a4aac0d967d83"
+#define LLVM_REPOSITORY "/home/maoh/riscv-llvm-src/out/llvm-project/llvm"
