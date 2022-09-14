1012514ddfe0be6c38372c3be80725aa043f20eb
Mao Han <han_mao@linux.alibaba.com>
2022-09-15 07:10:51 +0800

    Update clang toolchain
    Change-Id: I16f3bf8187d041cc5468bfe2e56bcae29cd1dd1c

--- clang-r416183c2/include/llvm/Support/VCSRevision.h
+++ clang-r416183c2/include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "364875daa894c1ba55bfe2761cb1e0c8d920fd07"
+#define LLVM_REVISION "326235ab9ad6155e60aaff22acfb7eb20eb77d21"
 #define LLVM_REPOSITORY "/home/maoh/riscv-llvm-src/out/llvm-project/llvm"
