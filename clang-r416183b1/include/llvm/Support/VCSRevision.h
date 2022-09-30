967c2732d56fa98369fd469c5f942e22562f171d
Mao Han <han_mao@linux.alibaba.com>
2022-09-30 17:00:41 +0800

    Update Clang for no-relax rollback

--- clang-r416183b1/include/llvm/Support/VCSRevision.h
+++ clang-r416183b1/include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "f1f566c3d0ff34cd8f841859372a4aac0d967d83"
+#define LLVM_REVISION "326235ab9ad6155e60aaff22acfb7eb20eb77d21"
 #define LLVM_REPOSITORY "/home/maoh/riscv-llvm-src/out/llvm-project/llvm"
